9bc6ca3d574bd2bb9a02e1597966acb34f447293
Chris <c.bailey00@hotmail.com>
2023-01-24 10:20:02 +0000

    changed from ternary operator that eslint didnt like

--- bank_transactions.js
+++ bank_transactions.js
@@ -8,8 +8,11 @@ class BankTransactions {
     this.balance += deposit
     this.balance -= withdrawal
 
-    deposit === '' ? deposit : deposit = deposit.toFixed(2)
-    withdrawal === '' ? withdrawal : withdrawal = withdrawal.toFixed(2)
+    if (deposit !== '') {
+      deposit = deposit.toFixed(2)
+    } else if (withdrawal !== '') {
+      withdrawal = withdrawal.toFixed(2)
+    }
 
     this.transactions.unshift({ date: new Date().toLocaleDateString(), deposit, withdrawal, balance: this.balance.toFixed(2) })
   }
